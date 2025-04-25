db8af03e627088e4b78df076534604f79707d75e
Huy Le <legiahuy050104@gmail.com>
2025-04-25 11:55:12 -0400

    Analytics added

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,5 +1,25 @@
 'use client'
 
+import { Analytics } from '@vercel/analytics/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
+
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
